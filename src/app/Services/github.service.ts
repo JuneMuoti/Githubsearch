d08d46e8618b74e90dbda6688c635146714409af
June Muoti <june.muoti@gmail.com>
2018-01-12 14:50:16 +0300

    add a service

---
+++ src/app/Services/github.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {Http,Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService {
+  private username:string;
+  private clientid ='8bd8e4a1593f6c592a96';
+  private clientsecret ='cf570f5e74bba802d6e191d0a275858ea2eef50c';
+
+  constructor(private http:http) {
+  console.log("service is now ready!");
+this.username ='JuneMuoti'
+ }
+ getGithubInfo ()
+ {
+   return this.http.get ("https.api.github.com/users" + this.username)
+ }
+
+}
